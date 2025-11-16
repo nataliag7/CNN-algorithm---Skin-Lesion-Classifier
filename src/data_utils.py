bf36a17b7bdd87e4df6e8c2031b635e6b9f280e8
nataliag7 <nataliagrabarska7@gmail.com>
2025-11-16 13:31:01 +0100

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,10 +6,7 @@ IMG_EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff", ".gif"}
 
 
 def scan_isic_dir(dirpath):
-    """
-    Return dicts {stem: path} for photos and masks in an ISIC-style folder.
-    Masks end with '_Segmentation' in the filename.
-    """
+    ##Return dicts {stem: path} for photos and masks in an ISIC-style folder. Masks end with '_Segmentation' in the filename.
     images, masks = {}, {}
     for fname in os.listdir(dirpath):
         ext = os.path.splitext(fname)[1].lower()
@@ -29,10 +26,7 @@ def scan_isic_dir(dirpath):
 
 
 def to_stem(s):
-    """
-    Convert an image filename (or path) to its base stem.
-    Also strips '_Segmentation' from mask filenames.
-    """
+    ##Convert an image filename (or path) to its base stem. Also strips '_Segmentation' from mask filenames.
     s = os.path.basename(str(s))
     stem = os.path.splitext(s)[0]
     if stem.endswith("_Segmentation"):
@@ -41,7 +35,7 @@ def to_stem(s):
 
 
 def decode_rgb(path):
-    """Read an RGB image from disk and return float32 tensor in [0,1]."""
+    #Read an RGB image from disk and return float32 tensor in [0,1].
     img = tf.io.read_file(path)
     img = tf.image.decode_image(img, channels=3, expand_animations=False)
     img = tf.image.convert_image_dtype(img, tf.float32)
@@ -49,7 +43,7 @@ def decode_rgb(path):
 
 
 def decode_mask(path):
-    """Read a 1-channel mask from disk and return float32 tensor in [0,1]."""
+    #Read a 1-channel mask from disk and return float32 tensor in [0,1].
     m = tf.io.read_file(path)
     m = tf.image.decode_image(m, channels=1, expand_animations=False)
     m = tf.image.convert_image_dtype(m, tf.float32)
@@ -57,11 +51,7 @@ def decode_mask(path):
 
 
 def crop_to_mask_with_margin(img, mask, has_mask, margin=0.10):
-    """
-    Crop image to the bounding box of the mask plus a margin (fraction of bbox size).
-    If there is no mask or it's empty, just return the original image.
-    """
-
+    ##Crop image to the bounding box of the mask plus a margin (fraction of bbox size). If there is no mask or it's empty, just return the original image.
     def _crop():
         #mask: (H, W, 1) -> (H, W)
         m = tf.squeeze(mask, -1)
